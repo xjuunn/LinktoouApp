39d1320e637746f0d160356ef50a4740880f8f67
xjuunn <145442547+xjuunn@users.noreply.github.com>
2024-07-15 12:18:32 +0800

    调整样式

--- components/UserInfoCard.vue/index.vue
+++ components/UserInfoCard.vue/index.vue
@@ -1,5 +1,6 @@
 <template>
-    <div class="userInfoCard m-1 rounded-lg hover:bg-base-300 transition-all collapse collapse-arrow" tabindex="0">
+    <div class="userInfoCard m-1 rounded-lg hover:bg-base-300 transition-color collapse collapse-arrow focus:border-base-content focus:border-2"
+        tabindex="0">
         <div class="flex collapse-title p-3 m-0">
             <div class="avatar skeleton" :class="UserInfoManager.isOnline.value ? 'online' : 'offline'">
                 <div class="w-10 h-10 rounded-full">
@@ -14,15 +15,15 @@
             <div class=" text-sm">
                 <div class="flex">
                     <b>ID: </b>
-                    <a @click="copy(UserInfoManager.id.value)" class="link link-hover overflow-hidden ms-2 me-2 text-neutral-400 text-sm flex-1">复制</a>
+                    <a @click="copy(UserInfoManager.id.value)"
+                        class="link link-hover overflow-hidden ms-2 me-2 text-neutral-400 text-sm flex-1">复制</a>
                     <b>RSA key: </b>
-                    <a @click="copy(UserInfoManager.public_rsa_key)" class="link link-hover overflow-hidden ms-2 me-2 text-neutral-400 text-sm flex-1">复制</a>
-                    
+                    <a @click="copy(UserInfoManager.public_rsa_key)"
+                        class="link link-hover overflow-hidden ms-2 me-2 text-neutral-400 text-sm flex-1">复制</a>
                 </div>
-                
             </div>
         </div>
-    </div>    
+    </div>
 </template>
 
 <script setup>
@@ -35,21 +36,15 @@ onMounted(() => {
     UserInfoManager.avatar_url.value = 'https://pbs.twimg.com/profile_images/1770095002783535104/QNTnQj0B_400x400.jpg'
     UserInfoManager.public_rsa_key = "tsetasdfserw";
     UserInfoManager.contact.value = {
-        phone:'1234567890',
-        email:'123123@gmail.com',
-        qq:'123123123',
-        wxid:'_x123123',
-        telegram:'@123123'
+        phone: '1234567890',
+        email: '123123@gmail.com',
+        qq: '123123123',
+        wxid: '_x123123',
+        telegram: '@123123'
     }
-
 })
-function test() {
-clipboard.copy('testaaaaa')
 
-
-}
-
-function copy(msg){
+function copy(msg) {
     clipboard.copy(msg);
 }
 
