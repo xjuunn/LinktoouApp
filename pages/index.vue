2ad1aeae6d74b95a6c160539aff1099e3d770404
xjuunn <145442547+xjuunn@users.noreply.github.com>
2024-07-16 18:02:02 +0800

    打开peer

--- pages/index.vue
+++ pages/index.vue
@@ -26,14 +26,14 @@ import AppNotification, { NotificationLevel } from '../types/AppNotification';
 import { AppNotificationBuilder } from '../utils/AppNotificationBuilder';
 
 
-// let { peer, connect, sendById } = usePeer();
+let { peer, connect, sendById } = usePeer();
 let { NotificationManager, createNotification } = useNotification();
 
-// peer.on('open', (id) => {
-//     UserInfoManager.id.value = id;
-//     UserInfoManager.isOnline.value = true;
-//     UserInfoManager.username.value = UserInfoManager.id.value.substring(0, 6)
-// })
+peer.on('open', (id) => {
+    UserInfoManager.id.value = id;
+    UserInfoManager.isOnline.value = true;
+    UserInfoManager.username.value = UserInfoManager.id.value.substring(0, 6)
+})
 onMounted(() => {
 
 })
